94ffc7e0c701aef8de35638fa09893c70c42343b
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-10-22 17:50:29 +0300

    ДЗ

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-x = 5
-x = 11
+try:
+    number_one = int(input("Введите первое число: "))
+    number_two = int(input("ВВедите второе число: "))
+    result = number_one / number_two
+except ValueError:
+    print("Ошибка: Введено не число!")
+except ZeroDivisionError:
+    print("Ошибка: Деление на ноль!")
+else:
+    print(result)
+finally:
+    print("Программа завершена")
