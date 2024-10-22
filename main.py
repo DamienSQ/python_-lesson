76df22718faa31081a6f33a2713aabd1dbea2691
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-10-22 18:45:21 +0300

    ДЗ

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 try:
     number_one = int(input("Введите первое число: "))
+    number_two = int(input("Введите второе число: "))
+    result = number_one + number_two
+except ValueError:
+    print("Ошибка: Введено не число!")
+else:
+    print(f"Результат сложения: {result}")
+
+try:
+    number_one = int(input("Введите первое число: "))
+    number_two = int(input("ВВедите второе число: "))
+    result = number_one - number_two
+except ValueError:
+    print("Ошибка: Введено не число!")
+else:
+    print(f"Результат вычитания: {result}")
+
+try:
+    number_one = int(input("Введите первое число: "))
+    number_two = int(input("ВВедите второе число: "))
+    result = number_one * number_two
+except ValueError:
+    print("Ошибка: Введено не число!")
+else:
+    print(f"Результат умножения: {result}")
+
+try:
+    number_one = int(input("Введите первое число: "))
     number_two = int(input("ВВедите второе число: "))
     result = number_one / number_two
 except ValueError:
@@ -7,6 +34,4 @@ except ValueError:
 except ZeroDivisionError:
     print("Ошибка: Деление на ноль!")
 else:
-    print(result)
-finally:
-    print("Программа завершена")
+    print(f"Результат деления: {result}")
