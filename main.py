b51e5ea4d4ac23defb05210d88d13a8d8d4519ce
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-11-05 20:37:29 +0300

    ДЗ

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-str_one = "\"random text\""
-str_two = "\'random text\'"
-str_three = "random text\\text"
-str_four = "random text\nnext"
+# 1 Задание
+def max_number(a, b):
+    result = max(a, b)
+    return result
 
-str_five = """"Random text" \'or\' not\t random\ntext"""
-print(str_one, str_two, str_three, str_four, str_five, sep='\n')
+
+max_number(23, 16)
+
+
+# 2 задание
+def empty_function():
+    pass
+
+
+# 3 задание
+def even_numbers(n):
+    random_numbers = []
+
+    for i in range(0, n + 1):
+        if i % 2 == 0:
+            random_numbers.append(i)
+            yield i
+
+
+for i in even_numbers(12):
+    print(i)
+
+
+# 4 задание
+def test_max_number():
+    assert max_number(24, 3) == 24, "Ошибка: должно быть число 24"
+    assert max_number(17, 36) == 36, "Ошибка: должно быть число 36"
+    assert max_number(15, 15) == 15, "Ошибка: должно быть число 15"
+
+
+test_max_number()
+print("Все тесты пройдены")
