3673b615ab07eff87eafc48d75d4f760424ea961
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-11-07 14:03:53 +0300

    ДЗ

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-age = 18
-citizen_country = True
-disqualified = False
+age = int(input("Введите ваш возраст: "))
+citizen_country = input("Вы являетесь гражданином страны? да/нет ")
+disqualified = input("Есть ли у вас судимости? да/нет ")
 
-if age >= 18 and citizen_country and not disqualified:
+if age >= 18 and citizen_country.lower() == "да" and disqualified.lower() == "нет":
     print("Вы можете голосовать на выборах")
 else:
     print("Вы не можете голосовать на выборах")
+
