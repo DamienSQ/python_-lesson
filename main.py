dad8e825060d7dac84cd38fb4ea22a64b8598cf5
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-11-16 22:36:37 +0300

    ДЗ

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import re
 
-user_text = input("Введите текст: ").strip().lower()
+
+def get_user_text():
+    text = input("Введите текст: ").strip().lower()
+    new_text = re.sub('[!?,.@$]', ' ', text).split()
+    return new_text
 
 
 def get_word_count(text: str):
-    new_text = re.sub('[!?,.@$]', ' ', text).split()
-    return print(f"В тексте {len(new_text)} слов/слово")
+    return print(f"В тексте {len(text)} слов/слово")
 
 
 def get_long_word(text: str):
-    new_text = re.sub('[!?,.@$]', ' ', text).split()
     longs_word = ""
 
-    for text in new_text:
-        if len(text) > len(longs_word):
-            longs_word = text
+    for word in text:
+        if len(word) > len(longs_word):
+            longs_word = word
 
     return print(f"Самое длинное слово - {longs_word}")
 
 
 def get_vowels(text: str):
     count_vowels = 0
+    join_text = ' '.join(text)
 
-    for char in text:
+    for char in join_text:
         if char in ('а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'):
             count_vowels += 1
 
@@ -30,18 +33,19 @@ def get_vowels(text: str):
 
 
 def get_repetition_word(text: str):
-    new_text = re.sub('[!?,.@$]', ' ', text).split()
     counts = {}
 
-    for word in new_text:
+    for word in text:
         if counts.get(word):
             counts[word] += 1
         else:
             counts[word] = 1
 
-    for count_key, coint_value in counts.items():
-        print(f"Слово \"{count_key}\" встречается {coint_value} раз/раза")
+    for count_key, count_value in counts.items():
+        print(f"Слово \"{count_key}\" встречается {count_value} раз/раза")
 
+
+user_text = get_user_text()
 
 get_word_count(user_text)
 get_long_word(user_text)
