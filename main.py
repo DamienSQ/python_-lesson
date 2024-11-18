1200ebe18341a4d74db56f92657f3c019bd8a525
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-11-18 23:21:33 +0300

    ДЗ Применение условий

--- main.py
+++ main.py
@@ -1,53 +1,27 @@
-import re
+# ДЗ №1
+def get_sum_even_number(number):
+    sum = 0
+    for num in range(0, number + 1, 2):
+        sum += num
+    return print(sum)
 
 
-def get_user_text():
-    text = input("Введите текст: ").strip().lower()
-    new_text = re.sub('[!?,.@$]', ' ', text).split()
-    return new_text
+# ДЗ №2
+def get_odd_numbers_square(number):
+    odd_numbers = [i ** 2 for i in range(0, number + 1) if i % 2 != 0]
+    return print(odd_numbers)
 
 
-def get_word_count(text: str):
-    return print(f"В тексте {len(text)} слов/слово")
+# ДЗ №3
+count_try = 0
+while True:
+    user_number = int(input("Введите число: "))
+    if user_number >= 0:
+        print("Попробуйте снова!")
+        count_try += 1
+    else:
+        print(f"Все верно, количество попыток - {count_try}")
+        break
 
-
-def get_long_word(text: str):
-    longs_word = ""
-
-    for word in text:
-        if len(word) > len(longs_word):
-            longs_word = word
-
-    return print(f"Самое длинное слово - {longs_word}")
-
-
-def get_vowels(text: str):
-    count_vowels = 0
-    join_text = ' '.join(text)
-
-    for char in join_text:
-        if char in ('а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'):
-            count_vowels += 1
-
-    return print(f"количество гласных - {count_vowels}")
-
-
-def get_repetition_word(text: str):
-    counts = {}
-
-    for word in text:
-        if counts.get(word):
-            counts[word] += 1
-        else:
-            counts[word] = 1
-
-    for count_key, count_value in counts.items():
-        print(f"Слово \"{count_key}\" встречается {count_value} раз/раза")
-
-
-user_text = get_user_text()
-
-get_word_count(user_text)
-get_long_word(user_text)
-get_vowels(user_text)
-get_repetition_word(user_text)
+get_sum_even_number(4)
+get_odd_numbers_square(7)
