024d92a9ed28cc1d80376c8a020969d6dbb45011
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-11-30 18:15:22 +0300

    ДЗ Сложение элементов списков

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-words_list = ["street", "coca-cola", "music", "game", "fun"]
+numbers_one = [1, 3, 5, 7]
+numbers_two = [2, 4, 6, 8, 2, 4]
 
-def change_item_list(list: list):
-    list[0], list[-1] = list[-1], list[0]
-    return print(list)
 
-change_item_list(words_list)
+def creat_new_list(numbers_one: list, numbers_two: list):
+    new_list = []
+    for num_one in numbers_one:
+        for num_two in numbers_two:
+            new_list.append(num_one + num_two)
+
+    return print(new_list)
+
+
+creat_new_list(numbers_one, numbers_two)
