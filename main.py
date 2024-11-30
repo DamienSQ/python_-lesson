996b6322b31380a4bae5f4c375b45475cced3c48
DamienSQ <Sidin.anatoliy@gmail.comdclear>
2024-11-30 16:49:06 +0300

    ДЗ замена элементов списка

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-# ДЗ №1
-def get_sum_even_number(number):
-    sum = 0
-    for num in range(0, number + 1, 2):
-        sum += num
-    return print(sum)
+words_list = ["street", "coca-cola", "music", "game", "fun"]
 
+def change_item_list(list: list):
+    list[0], list[-1] = list[-1], list[0]
+    return print(list)
 
-# ДЗ №2
-def get_odd_numbers_square(number):
-    odd_numbers = [i ** 2 for i in range(0, number + 1) if i % 2 != 0]
-    return print(odd_numbers)
-
-
-# ДЗ №3
-count_try = 0
-while True:
-    user_number = int(input("Введите число: "))
-    if user_number >= 0:
-        print("Попробуйте снова!")
-        count_try += 1
-    else:
-        print(f"Все верно, количество попыток - {count_try}")
-        break
-
-get_sum_even_number(4)
-get_odd_numbers_square(7)
+change_item_list(words_list)
